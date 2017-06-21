d868ede247bdb947fcc60dc48eb8e29edb3ba260
王振涛 <wangzhentao@zhiweicloud.com>
2017-06-21 15:05:03 +0800

    目录整合

---
+++ wyun-thrift/thrift/client/service.go
@@ -0,0 +1,68 @@
+package client
+
+import (
+	"fmt"
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"public-service/conf"
+	"time"
+	"wyun-thrift/thrift/gen-go/server"
+    "wyun-thrift/thrift/pool"
+)
+
+type service struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+	Pool *pool.Pool  `json:"-"`
+}
+
+var Service = &service{}
+
+func init() {
+	fmt.Println("init  hahahahah")
+	configByte, err := ioutil.ReadFile("config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf.TConfig = conf.T{}
+	err = yaml.Unmarshal(configByte, &conf.TConfig)
+	if nil != err {
+		log.Panic("thrift load config error: ", err)
+	}
+	// client
+    Service.Pool = &pool.Pool{
+		Dial: func() (interface{}, error) {
+
+			transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
+			protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+
+			transport, err := thrift.NewTSocket(net.JoinHostPort(Service.Host, Service.Port))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "error resolving address:", err)
+				os.Exit(1)
+			}
+
+			useTransport := transportFactory.GetTransport(transport)
+			client := server.NewMyServiceClientFactory(useTransport, protocolFactory)
+			if err := transport.Open(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error opening socket to"+Service.Host+":"+Service.Port, " ", err)
+				os.Exit(1)
+			}
+			return client, nil
+		},
+		Close: func(v interface{}) error {
+			v.(*server.MyServiceClient).Transport.Close()
+			return nil
+		},
+		MaxActive:   conf.TConfig.MaxConns,
+		MaxIdle:     conf.TConfig.MaxIdle,
+		IdleTimeout: time.Duration(conf.TConfig.MaxIdleConnDuration),
+	}
+
+	return
+}
