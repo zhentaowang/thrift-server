cc54c3e65a3a4f77140d92f003fba9692683e093
Jamy Luo <luojing@zhiweicloud.com>
2017-06-24 15:03:44 +0800

    add single server

--- server.go
+++ server.go
@@ -1,24 +1,39 @@
 package thriftserver
 
 import (
-	"fmt"
-	"os"
-	"git.apache.org/thrift.git/lib/go/thrift"
-	"code.aliyun.com/wyunshare/thrift-server/gen-go/server"
+    "fmt"
+    "os"
+    "git.apache.org/thrift.git/lib/go/thrift"
+    "code.aliyun.com/wyunshare/thrift-server/gen-go/server"
+    "code.aliyun.com/wyunshare/thrift-server/processor"
+    "code.aliyun.com/wyunshare/thrift-server/business"
 )
 
 func StartServer(address string, port string, handler server.MyService) {
 
-	processor := server.NewMyServiceProcessor(handler)
-	serverTransport, err := thrift.NewTServerSocket(address + ":" + port)
-	transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
-	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
-	if err != nil {
-		fmt.Println("Error!", err)
-		os.Exit(1)
-	}
+    processor := server.NewMyServiceProcessor(handler)
+    serverTransport, err := thrift.NewTServerSocket(address + ":" + port)
+    transportFactory := thrift.NewTFramedTransportFactory(thrift.NewTTransportFactory())
+    protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+    if err != nil {
+        fmt.Println("Error!", err)
+        os.Exit(1)
+    }
 
-	server := thrift.NewTSimpleServer4(processor, serverTransport, transportFactory, protocolFactory)
-	fmt.Println("thrift server in", address + ":" + port)
-	server.Serve()
+    server := thrift.NewTSimpleServer4(processor, serverTransport, transportFactory, protocolFactory)
+    fmt.Println("thrift server in", address + ":" + port)
+    server.Serve()
+}
+
+func StartSingleServer(address string, port string, serviceName string, businessService business.IBusinessService) {
+    businessServiceMap := &business.BusinessServiceMap{
+        ServiceMap: make(map[string]business.IBusinessService),
+    }
+    businessServiceMap.RegisterService(serviceName, businessService)
+
+    wyunServiceImpl := processor.WyunServiceImpl{
+        BusinessServiceMap: businessServiceMap,
+    }
+
+    StartServer(address, port, &wyunServiceImpl)
 }
--- server_test.go
+++ server_test.go
@@ -5,19 +5,34 @@ import (
 	"code.aliyun.com/wyunshare/thrift-server/business"
 	"code.aliyun.com/wyunshare/thrift-server/processor"
 	"code.aliyun.com/wyunshare/thrift-server/gen-go/server"
+	"encoding/json"
 )
 type BusinessServiceImpl struct {}
 func (msi *BusinessServiceImpl) Handle(operation string, paramJSON []byte) (r *server.Response, err error) {
-	return nil, nil
+	resMap := make(map[string]string)
+	resMap["operation"] = operation
+	resJSON, _ := json.Marshal(resMap)
+
+	res := server.Response{
+		ResponeCode: server.RESCODE__200,
+		ResponseJSON: resJSON,
+	}
+	return &res, nil
 }
 
 func TestStartServer(t *testing.T) {
 
 
-	businessServiceMap := &business.BusinessServiceMap{}
+	businessServiceMap := &business.BusinessServiceMap{
+		ServiceMap: make(map[string]business.IBusinessService),
+	}
 	businessServiceMap.RegisterService("businessService", &BusinessServiceImpl{})
 	wyunServiceImpl := processor.WyunServiceImpl{
 		BusinessServiceMap: businessServiceMap,
 	}
-	StartServer("localhost", "9092", &wyunServiceImpl)
+	go StartServer("localhost", "9092", &wyunServiceImpl)
+}
+
+func TestStartSingleServer(t *testing.T) {
+	go StartSingleServer("0.0.0.0", "9093", "businessService", &BusinessServiceImpl{})
 }
